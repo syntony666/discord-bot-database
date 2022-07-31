550a199d1cc6cfcc1501c1a77c102e2b448a1ff6
Tony <admin@syntony666.com>
2022-08-01 07:59:02 +0800

    add notif schema

---
+++ migrations/20220731215314-add_column_join_leave_message.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn('guilds', 'join_channel_id', {
+        type: Sequelize.STRING,
+        allowNull: true
+      }),
+      queryInterface.addColumn('guilds', 'join_message', {
+        type: Sequelize.STRING,
+        allowNull: true
+      }),
+      queryInterface.addColumn('guilds', 'leave_channel_id', {
+        type: Sequelize.STRING,
+        allowNull: true
+      }),
+      queryInterface.addColumn('guilds', 'leave_message', {
+        type: Sequelize.STRING,
+        allowNull: true
+      }),
+      
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
