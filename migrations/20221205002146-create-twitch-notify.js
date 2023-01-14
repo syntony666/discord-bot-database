251d0977a9d92a72d6a820f3351747135b1c877d
Tony <admin@syntony666.com>
2023-01-14 21:05:57 +0800

    add message

--- migrations/20221205002146-create-twitch-notify.js
+++ migrations/20221205002146-create-twitch-notify.js
@@ -2,49 +2,54 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('twitch_notifies', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      guild_id: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        references: {
-          model: 'guilds',
-          key: 'guild_id'
+    await queryInterface
+      .createTable('twitch_notifies', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onDelete: 'cascade'
-      },
-      channel_id: {
-        type: Sequelize.STRING
-      },
-      twitch_id: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        references: {
-          model: 'twitch_statuses',
-          key: 'twitch_id'
+        guild_id: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          references: {
+            model: 'guilds',
+            key: 'guild_id',
+          },
+          onDelete: 'cascade',
         },
-        onDelete: 'cascade'
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    }).then(() => {
-      return queryInterface.addConstraint('twitch_notifies',{
-        fields: ['guild_id', 'twitch_id'],
-        type: 'unique',
-        name: 'twitchNotifyIndex'
+        channel_id: {
+          type: Sequelize.STRING,
+        },
+        twitch_id: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          references: {
+            model: 'twitch_statuses',
+            key: 'twitch_id',
+          },
+          onDelete: 'cascade',
+        },
+        message: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(() => {
+        return queryInterface.addConstraint('twitch_notifies', {
+          fields: ['guild_id', 'twitch_id'],
+          type: 'unique',
+          name: 'twitchNotifyIndex',
+        });
       });
-    });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('twitch_notifies');
